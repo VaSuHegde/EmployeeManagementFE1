177b896986eb792c000ad5091ef6df161e9be844
Vasudha <vasudhahegde580@gmail.com>
2024-12-19 11:43:34 +0530

    first commit

---
+++ src/services/EmployeeService.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+// const EMPLOYEE_API_BASE_URL = "https://employeemanagementbe-j5yx.onrender.com";
+const EMPLOYEE_API_BASE_URL = "https://employeemanagementbe-rvhz.onrender.com";
+
+
+
+class EmployeeService {
+
+    getEmployees(){
+        return axios.get(EMPLOYEE_API_BASE_URL+'/'+"getAll");
+    }
+
+    createEmployee(employee){
+        return axios.post(EMPLOYEE_API_BASE_URL+'/'+"registerEmployee", employee);
+    }
+
+    getEmployeeById(employeeId){
+        return axios.get(`${EMPLOYEE_API_BASE_URL}/getById/${employeeId}`);
+
+    }
+
+    updateEmployee(employee, employeeId){
+        employee.id = employeeId;
+        return axios.put(EMPLOYEE_API_BASE_URL+'/'+"updateEmployee", employee);
+    }
+
+    deleteEmployee(employeeId){
+        return axios.delete(`${EMPLOYEE_API_BASE_URL}/deleteById/${employeeId}`);
+    }
+}
+
+export default new EmployeeService()
